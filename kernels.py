cfe170f0a685016c3a46c3ea331220dc5fc0cceb
bglbrt <82523827+bglbrt@users.noreply.github.com>
2022-03-25 12:19:52 +0100

    Update kernels.py

--- kernels.py
+++ kernels.py
@@ -111,3 +111,126 @@ class RBF():
 
         # return G
         return G
+
+# Exponential kernel
+class Exponential():
+    '''
+    Exponential kernel.
+
+    Arguments:
+        - sigma: float
+            bandwith parameter for the Exponential kernel
+    '''
+
+    def __init__(self, sigma=1.):
+
+        # set bandwith parameter
+        self.sigma = sigma
+
+    def compute(self, X1, X2):
+        '''
+        Kernel computation function.
+
+        Arguments:
+            - X1: np.array
+                N x d matrix
+            - X2: np.array
+                M x d matrix
+
+        Returns:
+            - G: np.array
+        '''
+
+        # compute norm of observations in X and Y
+        X1N = np.square(np.linalg.norm(X1, axis=1))
+        X2N = np.square(np.linalg.norm(X2, axis=1))
+
+        # compute ||xi-yi||^2 = ||xi||^2 + ||yi^2|| - 2 <xi, yi>
+        O = np.add.outer(X1N, X2N) - 2 * np.tensordot(X1, X2, axes=(1, 1))
+
+        # compute square root of O
+        OS = np.sqrt(O)
+
+        # compute Gram matrix
+        G = np.exp(-(1/(2 * (self.sigma**2))) * OS)
+
+        # return G
+        return G
+
+# Laplacian kernel
+class Laplacian():
+    '''
+    Laplacian kernel.
+
+    Arguments:
+        - sigma: float
+            bandwith parameter for the Laplacian kernel
+    '''
+
+    def __init__(self, sigma=1.):
+
+        # set bandwith parameter
+        self.sigma = sigma
+
+    def compute(self, X1, X2):
+        '''
+        Kernel computation function.
+
+        Arguments:
+            - X1: np.array
+                N x d matrix
+            - X2: np.array
+                M x d matrix
+
+        Returns:
+            - G: np.array
+        '''
+
+        # compute norm of observations in X and Y
+        X1N = np.square(np.linalg.norm(X1, axis=1))
+        X2N = np.square(np.linalg.norm(X2, axis=1))
+
+        # compute ||xi-yi||^2 = ||xi||^2 + ||yi^2|| - 2 <xi, yi>
+        O = np.add.outer(X1N, X2N) - 2 * np.tensordot(X1, X2, axes=(1, 1))
+
+        # compute square root of O
+        OS = np.sqrt(O)
+
+        # compute Gram matrix
+        G = np.exp(-(1/self.sigma) * OS)
+
+        # return G
+        return G
+
+# TanH kernel
+class TanH():
+    '''
+    Hyperbolic tangent kernel.
+    '''
+
+    def __init__(self, a, c):
+
+        # set polynomial parameters
+        self.a = a
+        self.c = c
+
+    def compute(self, X1, X2):
+        '''
+        Kernel computation function.
+
+        Arguments:
+            - X1: np.array
+                N x d matrix
+            - X2: np.array
+                M x d matrix
+
+        Returns:
+            - G: np.array
+                N x M Gram matrix
+        '''
+
+        # compute Gram matrix
+        G = np.tanh(a * np.tensordot(X, Y, axes=(1, 1)) + c)
+
+        # return G
+        return G
